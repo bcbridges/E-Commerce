b70ff1fec676fc9dfff446d6e404561fea328693
bcbridges <bcb35b@gmail.com>
2021-09-06 20:59:35 -0600

    Updated  products get-by-id route.

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -29,9 +29,28 @@ router.get("/", async (req, res) => {
 });
 
 // get one product
-router.get("/:id", (req, res) => {
+router.get("/:id", async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    res.json(
+      await Product.findByPk(req.params.id, {
+        include: [
+          {
+            model: Category,
+            attributes: ["category_name"],
+          },
+          {
+            model: Tag,
+            attributes: ["tag_name"],
+          },
+        ],
+      })
+    );
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 // create new product
